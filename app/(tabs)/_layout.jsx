279a8c5b9963e0f5919f940186d0a0b892e83942
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-01-23 14:50:20 -0700

    Add paper theme to nav

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Link, Tabs, Redirect } from 'expo-router';
 import { Pressable } from 'react-native';
 
-import Colors from '../../constants/Colors';
 import { useColorScheme } from 'react-native';
+import { useSession } from '../../global';
+
+import { LightTheme, DarkTheme } from '../_layout';
 
 function TabBarIcon(props) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -13,40 +15,58 @@ function TabBarIcon(props) {
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const navTheme = colorScheme === 'dark' ? DarkTheme : LightTheme;
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: true,
+        headerStyle: {
+          backgroundColor: navTheme.colors.card,
+        },
+        tabBarStyle: {
+          backgroundColor: navTheme.colors.card,
+          borderColor: navTheme.colors.card,
+          paddingBottom: 5,
+        },
+        tabBarActiveTintColor: navTheme.colors.primary,
+        headerTitleStyle: {
+          color: navTheme.colors.text,
+        },
+        tabBarInactiveTintColor: navTheme.colors.text,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Profile',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/profile" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          headerRight: headerBtnRight(),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="templates"
         options={{
-          title: 'Tab Two',
+          title: 'Templates',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
     </Tabs>
   );
 }
+
+function headerBtnRight() {
+  return () => (
+    <Link href="/profile">
+      <Pressable>
+        {({ pressed }) => (
+          <FontAwesome
+            name="info-circle"
+            size={25}
+            color={"#FFFF00"}
+            style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }} />
+        )}
+      </Pressable>
+    </Link>
+  );
+}
+
