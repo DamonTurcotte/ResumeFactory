c25f6bf64772239323d67dfb9b005bad3079d706
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-01-23 14:51:57 -0700

    Add profile data inputs, redux

--- app/profile.jsx
+++ app/profile.jsx
@@ -1,35 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button, Text, TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setName,
+  setEmail,
+  setPhone,
+  setLocation,
+  setWebsite,
+  setLinkedIn,
+  setGitHub
+} from "../redux/profileslice";
+import { useState } from "react";
+import { useRouter } from "expo-router";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+const ProfileScreen = () => {
+  const profile = useSelector((state) => state.profile);
+  const dispatch = useDispatch();
 
-export default function ProfileScreen() {
+  const [name, setNameInput] = useState(profile.Name);
+  const [email, setEmailInput] = useState(profile.Email);
+  const [phone, setPhoneInput] = useState(profile.Phone);
+  const [location, setLocationInput] = useState(profile.Location);
+  const [website, setWebsiteInput] = useState(profile.Website);
+  const [linkedIn, setLinkedInInput] = useState(profile.LinkedIn);
+  const [gitHub, setGitHubInput] = useState(profile.Github);
+
+  const theme = useTheme().colors;
+  const styles = getStyles(theme);
+  const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/profile.jsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+        <TextInput
+          label="Name"
+          value={name}
+          onChangeText={(text) => setNameInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={(text) => setEmailInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="Phone"
+          value={phone}
+          onChangeText={(text) => setPhoneInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="Location"
+          value={location}
+          onChangeText={(text) => setLocationInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="Website"
+          value={website}
+          onChangeText={(text) => setWebsiteInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="LinkedIn"
+          value={linkedIn}
+          onChangeText={(text) => setLinkedInInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <TextInput
+          label="GitHub"
+          value={gitHub}
+          onChangeText={(text) => setGitHubInput(text)}
+          style={styles.textInput}
+          mode="outlined"
+        />
+        <Button
+          mode="contained"
+          onPress={() => {
+            dispatch(setName(name));
+            dispatch(setEmail(email));
+            dispatch(setPhone(phone));
+            dispatch(setLocation(location));
+            dispatch(setWebsite(website));
+            dispatch(setLinkedIn(linkedIn));
+            dispatch(setGitHub(gitHub));
+            router.push("/");
+          }}
+        >
+          Save
+        </Button>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
+const getStyles = (theme) => ({
+  safe: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+  },
+  container: {
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  textInput: {
+    marginBottom: 10,
+  }
 });
+
+export default ProfileScreen;
