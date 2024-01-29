4336ff4de8f3b959205b140eae507107e2928b3a
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-01-28 21:08:13 -0700

    Store and persist combined reducers as root, add serialization middleware

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counter';
-import profileReducer from './profileslice';
+import ExpoFileSystemStorage from 'redux-persist-expo-filesystem';
+import profileReducer from './profileSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
 
-const store  = configureStore({
-  reducer: {
-    counter: counterReducer,
-    profile: profileReducer
-  },
+const persistConfig = {
+  key: 'root',
+  storage: ExpoFileSystemStorage,
+};
+
+const rootReducer = persistReducer(persistConfig, profileReducer);
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
