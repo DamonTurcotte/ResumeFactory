42b4d4f3421b19ed1010a2914ed60f144341de30
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-01-28 21:02:59 -0700

    Import dependencies

--- app/(tabs)/templates.jsx
+++ app/(tabs)/templates.jsx
@@ -1,9 +1,7 @@
-import { StyleSheet } from 'react-native';
-
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useTheme } from 'react-native-paper';
-import { Text } from 'react-native-paper';
+import { useTheme, Text } from 'react-native-paper';
+import { Stack } from 'expo-router';
 
 export default function TabTwoScreen() {
   const theme = useTheme().colors;
