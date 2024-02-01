dc7aed722bd812c2ecc515b19aae47ebe8ad1263
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-01 08:56:02 -0700

    Create initial experience screen

---
+++ app/profile/experience.jsx
@@ -0,0 +1,41 @@
+import { addJob, removeJob, editJob, setActive } from '../../redux/extraReducers/experienceSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useTheme } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Button, TextInput } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
+
+const ExperienceScreen = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const router = useRouter();
+  const jobs = useSelector(state => state.profiles[state.currentProfile].experience.Jobs);
+
+  console.log(jobs);
+  
+  return (
+    <SafeAreaView style={getStyles(theme).safe}>
+      <KeyboardAwareScrollView style={getStyles(theme).container}>
+
+
+
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+}
+
+const getStyles = (theme) => ({
+  safe: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: theme.colors.background,
+  },
+});
+
+export default ExperienceScreen;
