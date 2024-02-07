61bb31197e2bead6886fd645ad7bf761fa9e3971
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-07 01:44:41 -0700

    Remove gesture handler root view

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,10 @@
-import { Stack, useRouter } from 'expo-router';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { getFocusedRouteNameFromRoute, useNavigation, useRoute } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as NavigationBar from 'expo-navigation-bar';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
-import { StatusBar, useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { PaperProvider, ActivityIndicator } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { store, persistor } from '../redux/store';
 import { Provider } from 'react-redux';
@@ -23,7 +21,7 @@ export default function RootLayoutNav() {
   const colorScheme = useColorScheme();
   const theme = colorScheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme;
   NavigationBar.setBackgroundColorAsync("#000", true);
-  NavigationBar.setButtonStyleAsync("light")
+  NavigationBar.setButtonStyleAsync("light");
 
   SplashScreen.hideAsync();
 
@@ -31,27 +29,24 @@ export default function RootLayoutNav() {
     <PaperProvider theme={colorScheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme}>
       <Provider store={store}>
         <PersistGate persistor={persistor} loading={<ActivityIndicator animating={true} color={theme.colors.error} size='large' />}>
-          <GestureHandlerRootView style={{flex: 1}}>
-            <StatusBar barStyle={colorScheme === "dark" ? "light-content" : "dark-content"} backgroundColor={theme.colors.navbar} />
-            <Stack screenOptions={{
-              contentStyle: { backgroundColor: theme.colors.inverseOnSurface },
-              headerStyle: { backgroundColor: theme.colors.navbar},
-              headerTintColor: theme.colors.onNavbarVariant,
-              headerTitleStyle: { color: theme.colors.onNavbar, fontFamily: FONT.OrbitronB },
-              animation: "fade_from_bottom",
-            }}>
-              <Stack.Screen name="index" options={{title: "ResumeIO", headerShown: false, contentStyle: {backgroundColor: "#D0BCFF"}}} />
-              <Stack.Screen 
-                name="(tabs)" 
-                headerLeft={() => <HeaderBackButton path="/" theme={theme} />}
-                options={({ route }) => {
-                  const routeName = getFocusedRouteNameFromRoute(route) ?? 'default';
-                  let title = routeName[0].toUpperCase() + routeName.slice(1);
-                  return { title };
-                }}
-              />
-            </Stack>
-          </GestureHandlerRootView>
+          <Stack screenOptions={{
+            contentStyle: { backgroundColor: theme.colors.inverseOnSurface },
+            headerStyle: { backgroundColor: theme.colors.navbar},
+            headerTintColor: theme.colors.onNavbarVariant,
+            headerTitleStyle: { color: theme.colors.onNavbar, fontFamily: FONT.OrbitronB },
+            animation: "fade_from_bottom",
+          }}>
+            <Stack.Screen name="index" options={{title: "ResumeIO", headerShown: false, contentStyle: {backgroundColor: "#D0BCFF"}}} />
+            <Stack.Screen 
+              name="(tabs)" 
+              headerLeft={() => <HeaderBackButton path="/" theme={theme} />}
+              options={({ route }) => {
+                const routeName = getFocusedRouteNameFromRoute(route) ?? 'default';
+                let title = routeName[0].toUpperCase() + routeName.slice(1);
+                return { title };
+              }}
+            />
+          </Stack>
         </PersistGate>
       </Provider>
     </PaperProvider>
