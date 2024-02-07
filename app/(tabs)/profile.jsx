8f58c563190e15c862cee517da37e835f0bcf994
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-07 01:42:56 -0700

    Create profile cards, icons, add initial card grid

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,8 +1,9 @@
-import { Text, Modal, useTheme, Button, TextInput } from 'react-native-paper';
-import { View, StyleSheet, SafeAreaView, Pressable } from 'react-native';
-import { useRouter, Stack } from 'expo-router';
+import { Text, useTheme, Surface } from 'react-native-paper';
+import { View, SafeAreaView, Pressable } from 'react-native';
+import { useRouter } from 'expo-router';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useState, useRef } from 'react';
+import { useRef } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { sliceActivators } from '../../redux/extraReducers';
 import { PressableDragAndDrop } from '../../components';
@@ -25,12 +26,14 @@ export default ProfileScreen = () => {
 
       <View style={styles.container}>
         <View style={styles.subcontainer}>
+
           <Text style={styles.title} variant='titleLarge'>Active</Text>
           <View style={styles.categories} ref={activeRef}>
             <Pressable onPress={() => router.navigate('/profile/personal')}>
-              <View style={{backgroundColor: theme.primary}}>
-                <Text variant='bodyLarge'>Personal</Text>
-              </View>
+              <ProfileCard
+                category={"personal"}
+                draggable={false}
+              />
             </Pressable>
             { activeCategories.map((category) => (
                 <PressableDragAndDrop
@@ -39,11 +42,10 @@ export default ProfileScreen = () => {
                   onRelease={() => dispatch(sliceActivators[category](false))}
                   targetRef={inactiveRef}
                 >
-                  <View style={{backgroundColor: theme.primary}}>
-                    <Text variant='bodyLarge'>
-                      {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </Text>
-                  </View>
+                  <ProfileCard
+                    category={category}
+                    draggable={true}
+                  />
                 </PressableDragAndDrop>
               )
             )}
@@ -59,11 +61,10 @@ export default ProfileScreen = () => {
                   onRelease={() => dispatch(sliceActivators[category](true))}
                   targetRef={activeRef}
                 >
-                  <View style={{backgroundColor: theme.primary}}>
-                    <Text variant='bodyLarge'>
-                      {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </Text>
-                  </View>
+                  <ProfileCard
+                    category={category}
+                    draggable={true}
+                  />
                 </PressableDragAndDrop>
               )
             })}
@@ -74,6 +75,51 @@ export default ProfileScreen = () => {
   );
 }
 
+const ProfileCard = ({ category, draggable }) => {
+  const theme = useTheme().colors;
+  const styles = getStyles(theme);
+  const cardIcon = cardIcons(theme)[category];
+
+  return (
+    <Surface
+      elevation={3}
+      style={styles.cardContainer}
+      mode='elevated'
+    >
+      <View style={styles.cardInfo}>
+        {cardIcon}
+        <Text
+          variant='bodyLarge'
+        >
+          {category.charAt(0).toUpperCase() + category.slice(1)}
+        </Text>
+      </View>
+      { draggable === true &&
+        <MaterialCommunityIcons
+          name="drag"
+          style={styles.dragIcon}
+          size={32}
+          color={theme.background}
+        />
+      }
+    </Surface>
+  )
+};
+
+const cardIcons = (theme) => ({
+  personal: (<MaterialCommunityIcons name="account" size={24} color={theme.primary} />),
+  objective: (<MaterialCommunityIcons name="bullhorn" size={24} color={theme.primary} />),
+  summary: (<MaterialCommunityIcons name="note-text" size={24} color={theme.primary} />),
+  experience: (<MaterialCommunityIcons name="briefcase" size={24} color={theme.primary} />),
+  education: (<MaterialCommunityIcons name="school" size={24} color={theme.primary} />),
+  skills: (<MaterialCommunityIcons name="pencil" size={24} color={theme.primary} />),
+  certificates: (<MaterialCommunityIcons name="certificate" size={24} color={theme.primary} />),
+  projects: (<MaterialCommunityIcons name="folder" size={24} color={theme.primary} />),
+  publications: (<MaterialCommunityIcons name="newspaper" size={24} color={theme.primary} />),
+  languages: (<MaterialCommunityIcons name="translate" size={24} color={theme.primary} />),
+  references: (<MaterialCommunityIcons name="account-group" size={24} color={theme.primary} />),
+})
+
 const getStyles = (theme) => ({
   safe: {
     flex: 1,
@@ -82,22 +128,45 @@ const getStyles = (theme) => ({
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingHorizontal: 6
   },
   subcontainer: {
-    width: "90%"
+    width: "100%",
   },
   categories: {
-    padding: 10,
+    padding: 3,
     borderRadius: 10,
+    backgroundColor: "transparent",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: "100%",
+    minHeight: 84,
+    borderColor: theme.outline,
     borderWidth: 1,
-    borderColor: theme.primary,
-    height: 200,
+    justifyContent: 'space-evenly',
   },
   title: {
     fontSize: 20,
     textAlign: 'center',
   },
-  button: {
-    marginTop: 0,
+  cardContainer: {
+    paddingHorizontal: 0,
+    paddingVertical: 8,
+    borderRadius: 8,
+    backgroundColor: theme.inversePrimary,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 110,
+    height: 70,
+    margin: 3,
   },
+  cardInfo: {
+    alignItems: 'center'
+  },
+  dragIcon: {
+    position: 'absolute',
+    right: -3,
+    top: -1
+  }
 });
