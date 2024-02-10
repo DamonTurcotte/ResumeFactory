7b399d01631c0ec24b7051885ac32c9b37ee4d53
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-09 20:58:39 -0700

    Add logix, inputs, data dispatch

--- app/profile/objective.jsx
+++ app/profile/objective.jsx
@@ -1,12 +1,22 @@
-import { useTheme } from 'react-native-paper';
+import { useTheme, TextInput, Button } from 'react-native-paper';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setObjective } from '../../redux/extraReducers/objectiveSlice';
 
 
 export default ObjectiveScreen = () => {
+  const objective = useSelector((state) => state.profiles[state.currentProfile].objective);
+  const [objectiveInput, setObjectiveInput] = useState(objective.data);
+
   const theme = useTheme();
   const styles = getStyles(theme);
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+  const profileid = useSelector((state) => state.currentProfile);
   
   return (
     <SafeAreaView style={getStyles(theme).safe}>
@@ -15,9 +25,25 @@ export default ObjectiveScreen = () => {
         headerLeft={() => <HeaderBackButton path="profile" theme={theme} />}
       />
       <KeyboardAwareScrollView style={styles.container}>
-
-
-
+        <TextInput
+          style={styles.textInput}
+          label='Objective'
+          value={objectiveInput}
+          onChangeText={setObjectiveInput}
+          mode="outlined"
+          multiline={true}
+          numberOfLines={5}
+          placeholder='Enter your objective here.'
+        />
+        <Button
+          mode="contained"
+          onPress={() => {
+            dispatch(setObjective({profileid, data: objectiveInput}));
+            router.navigate('profile');
+          }}
+        >
+          Save
+        </Button>
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
@@ -33,4 +59,7 @@ const getStyles = (theme) => ({
     padding: 20,
     backgroundColor: theme.colors.background,
   },
+  textInput: {
+    marginBottom: 20,
+  }
 });
--- app/profile/summary.jsx
+++ app/profile/summary.jsx
@@ -1,12 +1,22 @@
-import { useTheme } from 'react-native-paper';
+import { useTheme, TextInput, Button } from 'react-native-paper';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSummary } from '../../redux/extraReducers/summarySlice';
 
 
 export default SummaryScreen = () => {
+  const summary = useSelector((state) => state.profiles[state.currentProfile].summary);
+  const [summaryInput, setSummaryInput] = useState(summary.data);
+
   const theme = useTheme();
   const styles = getStyles(theme);
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+  const profileid = useSelector((state) => state.currentProfile);
   
   return (
     <SafeAreaView style={getStyles(theme).safe}>
@@ -15,9 +25,25 @@ export default SummaryScreen = () => {
         headerLeft={() => <HeaderBackButton path="profile" theme={theme} />}
       />
       <KeyboardAwareScrollView style={styles.container}>
-
-
-
+        <TextInput
+          style={styles.textInput}
+          label='Summary'
+          value={summaryInput}
+          onChangeText={setSummaryInput}
+          mode="outlined"
+          multiline={true}
+          numberOfLines={5}
+          placeholder='Enter your professional summary here.'
+        />
+        <Button
+          mode="contained"
+          onPress={() => {
+            dispatch(setSummary({profileid, data: summaryInput}));
+            router.navigate('profile');
+          }}
+        >
+          Save
+        </Button>
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
@@ -33,4 +59,7 @@ const getStyles = (theme) => ({
     padding: 20,
     backgroundColor: theme.colors.background,
   },
+  textInput: {
+    marginBottom: 20,
+  }
 });
