9e829755f24ce2f1d586d01b73509531fb6653b8
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-11 19:31:55 -0700

    Add certificate screen form and logic

--- app/profile/certificates.jsx
+++ app/profile/certificates.jsx
@@ -1,12 +1,58 @@
-import { useTheme } from 'react-native-paper';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack } from 'expo-router';
+import { useState } from 'react';
+import { View, SafeAreaView } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Text, TextInput, Button, useTheme } from 'react-native-paper';
 
+import { HeaderBackButton } from '../../components/headerBackButton';
+import { DeleteModal } from '../../components/deleteModal';
+import { CertificateCard } from '../../components/cards/certificateCard';
+import { addCertificate, removeCertificate, setCertificate } from '../../redux/extraReducers/certificateSlice'; 
 
-export default CertificatesScreen = () => {
+const CertificateScreen = () => {
+  const [certificateEditor, setCertificateEditor] = useState(false);
+  const [index, setIndex] = useState(-1);
+
+  const [certificate, setCertificate] = useState("");
+  const [issuer, setIssuer] = useState("");
+  const [issueDate, setIssueDate] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [credentialID, setCredentialID] = useState("");
+  const [credentialURL, setCredentialURL] = useState("");
+
+  const [deleteIndex, setDeleteIndex] = useState(-1);
+
+  const certificates = useSelector(state => state.profiles[state.currentProfile].certificates.data);
+
+  const dispatch = useDispatch();
   const theme = useTheme();
   const styles = getStyles(theme);
+
+  const handleCertificateEditor = () => setCertificateEditor(!certificateEditor);
+
+  const saveCertificateData = () => {
+    let data = {
+      Certificate: certificate,
+      Issuer: issuer,
+      IssueDate: issueDate,
+      ExpirationDate: expirationDate,
+      CredentialID: credentialID,
+      CredentialURL: credentialURL,
+    };
+    if (index === -1) {
+      dispatch(addCertificate({ data: data }));
+    } else {
+      dispatch(setCertificate({ index: index, data: data }));
+    }
+    setCertificate("");
+    setIssuer("");
+    setIssueDate("");
+    setExpirationDate("");
+    setCredentialID("");
+    setCredentialURL("");
+    handleCertificateEditor();
+  };
   
   return (
     <SafeAreaView style={getStyles(theme).safe}>
@@ -15,9 +61,99 @@ export default CertificatesScreen = () => {
         headerLeft={() => <HeaderBackButton path="profile" theme={theme} />}
       />
       <KeyboardAwareScrollView style={styles.container}>
-
-
-
+        { certificateEditor ? (
+          <View>
+            <TextInput
+              mode='outlined'
+              label="Certificate"
+              value={certificate}
+              onChangeText={text => setCertificate(text)}
+            />
+            <TextInput
+              mode='outlined'
+              label="Issuer"
+              value={issuer}
+              onChangeText={text => setIssuer(text)}
+            />
+            <TextInput
+              mode='outlined'
+              label="Issue Date"
+              value={issueDate}
+              onChangeText={text => setIssueDate(text)}
+            />
+            <TextInput
+              mode='outlined'
+              label="Expiration Date"
+              value={expirationDate}
+              onChangeText={text => setExpirationDate(text)}
+            />
+            <TextInput
+              mode='outlined'
+              label="Credential ID"
+              value={credentialID}
+              onChangeText={text => setCredentialID(text)}
+            />
+            <TextInput
+              mode='outlined'
+              label="Credential URL"
+              value={credentialURL}
+              onChangeText={text => setCredentialURL(text)}
+            />
+            <Button
+              mode="contained"
+              style={styles.button}
+              onPress={saveCertificateData}
+            >
+              Save
+            </Button>
+          </View>
+        ) : (
+          <>
+          { certificates.map((data, i) => (
+            <CertificateCard data={data} key={i}>
+              <Button
+                mode='text'
+                textColor={theme.colors.onBackgroundVariant}
+                onPress={() => {
+                  setIndex(i);
+                  setCertificate(data.Certificate);
+                  setIssuer(data.Issuer);
+                  setIssueDate(data.IssueDate);
+                  setExpirationDate(data.ExpirationDate);
+                  setCredentialID(data.CredentialID);
+                  setCredentialURL(data.CredentialURL);
+                  handleCertificateEditor();
+              }}>
+                Edit
+              </Button>
+              <Button
+                mode='text'
+                textColor={theme.colors.onBackgroundVariant}
+                onPress={() => setDeleteIndex(i)}
+              >
+                Delete
+              </Button>
+            </CertificateCard>
+          ))}
+          <Button
+            mode="contained"
+            style={styles.button}
+            onPress={handleCertificateEditor}
+          >
+            Add Certificate
+          </Button>
+          { deleteIndex !== -1 && (
+            <DeleteModal
+              visible={deleteIndex !== -1}
+              hideModal={() => setDeleteIndex(-1)}
+              deleteItem={() => {
+                dispatch(removeCertificate({ index: deleteIndex }));
+                setDeleteIndex(-1);
+              }}
+            />
+          )}
+          </>
+        )}
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
@@ -33,4 +169,9 @@ const getStyles = (theme) => ({
     padding: 20,
     backgroundColor: theme.colors.background,
   },
+  button: {
+    marginTop: 10,
+  }
 });
+
+export default CertificateScreen;
