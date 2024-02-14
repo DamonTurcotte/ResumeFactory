1c79a600630686f8839ae3039c270903ede5cad0
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-14 07:54:31 -0700

    Modify card font sizes, order of data fields

--- components/cards/schoolCard.jsx
+++ components/cards/schoolCard.jsx
@@ -5,11 +5,11 @@ export const SchoolCard = ({ data, style, children }) => {
   return (
     <Card mode="outlined" style={style}>
       <Card.Content>
-        <Text variant="headlineSmall">{data.School}</Text>
-        <Text variant="bodyLarge">{data.Credential}</Text>
-        <Text variant="bodyLarge">{data.Start} {data.Start.length > 0 && data.End.length > 0 && "-"} {data.End}</Text>
-        <Text variant="bodyLarge">{data.GPA}</Text>
-        <Text variant="bodyLarge">{data.Description}</Text>
+        <Text variant="headlineSmall">{data.Credential}</Text>
+        <Text variant="titleLarge">{data.School}</Text>
+        <Text variant="bodyMedium">{data.Start} {data.Start.length > 0 && data.End.length > 0 && "-"} {data.End}</Text>
+        <Text variant="bodyMedium">{data.GPA} GPA</Text>
+        <Text variant="bodySmall">{data.Description}</Text>
       </Card.Content>
       <Card.Actions>
         {children}
