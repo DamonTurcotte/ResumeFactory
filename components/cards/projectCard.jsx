8039a6d9fd5eda5659438a88cbfd7b994105a595
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-14 07:54:51 -0700

    Modify font sizes, add margin

--- components/cards/projectCard.jsx
+++ components/cards/projectCard.jsx
@@ -3,14 +3,14 @@ import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 
 export const ProjectCard = ({ project, index, handleEdit, setDelete, style, buttonColor="gold" }) => {
-
+  const theme = useTheme();
   return (
     <Card mode="outlined" style={style}>
       <Card.Content>
         <Text variant="headlineSmall">{project.Title}</Text>
-        <Text variant="bodyLarge">{project.Category}</Text>
-        <Text variant="bodyLarge">{project.Start} - {project.End}</Text>
-        <Text variant="bodyMedium">{project.Description}</Text>
+        <Text variant="titleLarge">{project.Category}</Text>
+        <Text variant="bodyMedium">{project.Start} - {project.End}</Text>
+        <Text variant="bodySmall" style={{marginTop: 2}}>{project.Description}</Text>
       </Card.Content>
       <Card.Actions>
         <Button mode="text" textColor={buttonColor} onPress={() => handleEdit(index)}>Edit</Button>
