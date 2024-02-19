3588fc2d3155c38ab7377120c5c7510f3a41c6a7
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-02-18 23:20:42 -0700

    Add settings route and header button

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,10 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import * as NavigationBar from 'expo-navigation-bar';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 import { useColorScheme } from 'react-native';
-import { PaperProvider, ActivityIndicator } from 'react-native-paper';
+import { PaperProvider, ActivityIndicator, IconButton } from 'react-native-paper';
 
 import { store, persistor } from '../redux/store';
 import { Provider } from 'react-redux';
@@ -12,6 +12,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { CombinedDefaultTheme, CombinedDarkTheme } from '../configs';
 import { FONT } from '../constants';
+import { HeaderBackButton } from '../components';
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -23,27 +24,33 @@ export default function RootLayoutNav() {
   NavigationBar.setBackgroundColorAsync("#000", true);
   NavigationBar.setButtonStyleAsync("light");
 
+  const router = useRouter();
+
   SplashScreen.hideAsync();
 
   return (
     <PaperProvider theme={colorScheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme}>
       <Provider store={store}>
         <PersistGate persistor={persistor} loading={<ActivityIndicator animating={true} color={theme.colors.error} size='large' />}>
-          <Stack screenOptions={{
-            contentStyle: { backgroundColor: theme.colors.inverseOnSurface },
-            headerStyle: { backgroundColor: theme.colors.navbar},
-            headerTintColor: theme.colors.onNavbarVariant,
-            headerTitleStyle: { color: theme.colors.onNavbar, fontFamily: FONT.OrbitronB },
-            animation: "fade_from_bottom",
-          }}>
+          <Stack
+            screenOptions={{
+              contentStyle: { backgroundColor: theme.colors.inverseOnSurface },
+              headerStyle: { backgroundColor: theme.colors.navbar},
+              headerTintColor: theme.colors.onNavbarVariant,
+              headerTitleStyle: { color: theme.colors.onNavbar, fontFamily: FONT.OrbitronB },
+              animation: "fade_from_bottom",
+            }}
+          >
             <Stack.Screen name="index" options={{title: "ResumeIO", headerShown: false, contentStyle: {backgroundColor: "#D0BCFF"}}} />
+            <Stack.Screen name="settings" options={{title: "Settings"}} />
             <Stack.Screen 
               name="(tabs)" 
               headerLeft={() => <HeaderBackButton path="/" theme={theme} />}
               options={({ route }) => {
                 const routeName = getFocusedRouteNameFromRoute(route) ?? 'default';
                 let title = routeName[0].toUpperCase() + routeName.slice(1);
-                return { title };
+                headerRight = () => <IconButton icon="cog" onPress={() => router.navigate('/settings')} />;
+                return { title, headerRight };
               }}
             />
           </Stack>
