19ff5dbf0644665ebdf82ecc7608a0c093fff38d
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-04-15 04:44:26 -0600

    Add background color to text inputs

--- components/inputs/textArea.jsx
+++ components/inputs/textArea.jsx
@@ -1,14 +1,18 @@
-import { TextInput } from "react-native-paper";
+import { TextInput, useTheme } from "react-native-paper";
 
-export const TextArea = ({label, value, onChangeText, style={}, placeholder=null}) => (
-  <TextInput
-    label={label}
-    value={value}
-    onChangeText={onChangeText}
-    mode="outlined"
-    multiline={true}
-    numberOfLines={4}
-    style={[{ marginVertical: 3 }, style]}
-    placeholder={placeholder}
-  />
-);
+export const TextArea = ({label, value, onChangeText, style={}, placeholder=null}) => {
+  const theme = useTheme();
+
+  return (
+    <TextInput
+      label={label}
+      value={value}
+      onChangeText={onChangeText}
+      mode="outlined"
+      multiline={true}
+      numberOfLines={4}
+      style={[{ marginVertical: 3, backgroundColor: theme.colors.backdrop }, style]}
+      placeholder={placeholder}
+    />
+  );
+}
--- components/inputs/textBox.jsx
+++ components/inputs/textBox.jsx
@@ -1,22 +1,26 @@
-import { TextInput } from "react-native-paper";
+import { TextInput, useTheme } from "react-native-paper";
 
-export const TextBox = ({label, value, onChangeText, style=null, close=null, textContentType="none", inputMode=null, placeholder="" }) => (
+export const TextBox = ({label, value, onChangeText, style=null, close=null, textContentType="none", inputMode=null, placeholder="" }) => {
+  const theme = useTheme();
+
+  return (
   <TextInput
     label={label}
     value={value}
     onChangeText={onChangeText}
     mode="outlined"
-    style={style}
     textContentType={textContentType}
     inputMode={inputMode}
     placeholder={placeholder}
+    contentStyle={{backgroundColor: theme.colors.backdrop}}
+    style={style}
     right={close !== null ? <TextInput.Icon
-        forceTextInputFocus={false}
-        icon="close"
-        hitSlop={{top: 5, bottom: 5, left: 5, right: 5}}
-        size={16}
-        onPress={close}
+      forceTextInputFocus={false}
+      icon="close"
+      hitSlop={{top: 5, bottom: 5, left: 5, right: 5}}
+      size={16}
+      onPress={close}
       /> : undefined
     }
   />
-);
+);}
