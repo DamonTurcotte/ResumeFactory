688b1db9974ccddef2767cb16138bbb89bdfb200
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-05-06 07:46:53 -0600

    Refactor for persistant template options through redux

--- templates/templateView.jsx
+++ templates/templateView.jsx
@@ -6,17 +6,15 @@ import { useSelector } from "react-redux";
 import { getFont, getFontFaces } from "./fontHooks";
 import { templateVariants } from "./variants";
 
-import templateDecorations from "./decorations";
-
-export const TemplateView = ({ variant, fonts, fontSize, margin, style, pages, setPages, setHtml, print, setPrint }) => {
+export const TemplateView = ({ variant, fonts, style }) => {
   const [font1] = getFont(fonts[0]);
   const [font2] = fonts.length > 1 ? getFont(fonts[1]) : [null];
   const [font3] = fonts.length > 2 ? getFont(fonts[2]) : [null];
 
   const profile = useSelector((state) => state.profiles[state.currentProfile]);
-  const options = useSelector((state) => state.profiles[state.currentProfile].options);
 
-  const [categoryOrder, setCategoryOrder] = useState(Object.entries(profile).filter(([key, value]) => value.active).map(([key, value]) => key));
+  const { fontSize } = profile.options.templateOptions[variant];
+
   const [webViewDimensions, setWebViewDimensions] = useState({ width: 0, height: 0 });
 
   const viewRef = useRef(null);
@@ -25,18 +23,17 @@ export const TemplateView = ({ variant, fonts, fontSize, margin, style, pages, s
     letter: { width: 8.5, height: 11 },
     A4: { width: 8.27, height: 11.69 },
   };
+  
+  const { html, css, columns, extraPageHtml } = templateVariants[variant](profile, fonts, templateViewDimensions[profile.options.size]);
 
   const onLayout = useCallback(event => {
     viewRef.current.measure((x, y, width, height, pageX, pageY) => {
-      const aspectRatio = templateViewDimensions[options.size].width / templateViewDimensions[options.size].height;
+      const aspectRatio = templateViewDimensions[profile.options.size].width / templateViewDimensions[profile.options.size].height;
       const newWidth = Math.min(width, height * aspectRatio);
       const newHeight = newWidth / aspectRatio;
       setWebViewDimensions({ width: newWidth, height: newHeight });
     }
-  )}, [options.size]);
-
-  // templateData = { html, css, columns }
-  const { html, css, columns, extraPageHtml } = templateVariants[variant](profile, fonts, fontSize, margin, templateViewDimensions[options.size], categoryOrder, setCategoryOrder, pages, setPages);
+  )}, [profile.options.size]);
 
   const compactMode = {
     summary: false,
@@ -68,7 +65,7 @@ export const TemplateView = ({ variant, fonts, fontSize, margin, style, pages, s
             margin: 0;
           }
           html { font-family: '${fonts[0]}'; font-size: ${fontSize}px; }
-          body { width: ${templateViewDimensions[options.size].width}in; height: ${templateViewDimensions[options.size].height * pages}in; }
+          body { width: ${templateViewDimensions[profile.options.size].width}in; }
           ${css}
         </style>
       </head>
@@ -110,10 +107,10 @@ export const TemplateView = ({ variant, fonts, fontSize, margin, style, pages, s
                 document.body.innerHTML += extraPageHtml;
                 const aside = document.querySelectorAll("aside")[page];
                 aside.style.position = "absolute";
-                aside.style.top = \`calc(${templateViewDimensions[options.size].height}in * \$\{page\})\`;
+                aside.style.top = \`calc(${templateViewDimensions[profile.options.size].height}in * \$\{page\})\`;
                 aside.style.left = "0";
-                aside.style.width = "${templateViewDimensions[options.size].width}in";
-                aside.style.height = "${templateViewDimensions[options.size].height}in";
+                aside.style.width = "${templateViewDimensions[profile.options.size].width}in";
+                aside.style.height = "${templateViewDimensions[profile.options.size].height}in";
                 aside.style.zIndex = "-1";
                 aside.style.pointerEvents = "none";
 
@@ -134,11 +131,6 @@ export const TemplateView = ({ variant, fonts, fontSize, margin, style, pages, s
 
     </html>
   `;
-
-  if (print) {
-    setHtml(buildTemplateHtml());
-    setPrint(false);
-  }
 
   return (
     <View
