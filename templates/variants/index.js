a7cd707d35d1cdf3c8805fc5ed7848a8f4961ee2
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-05-06 07:37:03 -0600

    Export template colors and reordering functions

--- templates/variants/index.js
+++ templates/variants/index.js
@@ -1,5 +1,19 @@
-import { variant0 } from "./0";
+import { variant0, colorScheme0, editSectionOrder0 } from "./0";
 
-export const templateVariants = {
+const templateVariants = {
   0: variant0,
+};
+
+const templateColors = {
+  0: colorScheme0,
+};
+
+const templateReorders = {
+  0: editSectionOrder0,
+};
+
+export {
+  templateVariants,
+  templateColors,
+  templateReorders,
 };
