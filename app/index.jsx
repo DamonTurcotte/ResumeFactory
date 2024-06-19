5fe8143d8fa6f99501f4e49a9a0a4b9a3b189612
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:26:06 -0600

    Add new logo, animation, file selector, file creation buttons

--- app/index.jsx
+++ app/index.jsx
@@ -1,94 +1,44 @@
-import { ScrollView, View } from "react-native";
-import { Text, TextInput, Button, useTheme } from "react-native-paper";
+import { ScrollView, View, Dimensions, Animated, Easing, Pressable } from "react-native";
+import { Text, TextInput, Button, useTheme, TouchableRipple } from "react-native-paper";
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addProfile, setCurrentProfile } from '../redux/profileSlice';
+import { addProfile, setCurrentProfile } from '../redux/profile';
 import { FontAwesome } from '@expo/vector-icons';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { FONT } from "../constants";
+import { Icon, Logo, CreateFileButton, FileSelect } from "../components";
 
 export default App = () => {
-  const router = useRouter();
   const theme = useTheme();
-  const styles = getStyles(theme)
-  const dispatch = useDispatch();
-  const profiles = useSelector((state) => state.profiles);
-  const profileOrder = useSelector((state) => state.order);
-  const [newProfile, setNewProfile] = useState(false);
-  const [profileID, setProfileID] = useState("");
-
-  const toggleNewProfile = () => {
-    setNewProfile(!newProfile);
-  }
-
-  const submitProfile = () => {
-    if (profileID.length > 0) {
-      dispatch(addProfile(profileID));
-    }
-    setProfileID("");
-    toggleNewProfile();
-  }
+  const styles = getStyles(theme);
+  const router = useRouter();
 
   return (
     <SafeAreaView style={styles.safe}>
-      <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>
-            Resume
-          </Text>
-          <Text style={[styles.title, {color: theme.colors.logo}]}>
-            IO
-          </Text>
+      <ScrollView
+        contentContainerStyle={{
+          marginTop: 10,
+        }}
+      >
+        <Logo />
+        <View style={styles.container}>
+          <CreateFileButton
+            title1="Build a"
+            title2="New Resume"
+            icon="resume"
+            onPress={() => console.log("Build a new resume")}
+          />
+          <CreateFileButton
+            title1="Write a"
+            title2="Cover Letter"
+            icon="letter"
+            onPress={() => console.log("Write a cover letter")}
+          />
+          <FileSelect />
         </View>
-        
-        <ScrollView
-          style={styles.scroll}
-          contentContainerStyle={styles.scrollContent}
-        >
-
-        {profileOrder.map((profile, index) => (
-          <Button
-            key={index}
-            mode="contained-tonal"
-            style={styles.button}
-            labelStyle={styles.buttonLabel}
-            contentStyle={styles.buttonContent}
-            icon={() => <FontAwesome name="user-circle" size={30} color={theme.colors.onPrimary} />}
-            onPress={() => {
-              dispatch(setCurrentProfile(profile));
-              router.navigate("profile");
-            }}
-          >
-            {profile}
-          </Button>
-        ))}
-
-        { newProfile ? (
-          <TextInput
-            value={profileID}
-            onChangeText={(text) => setProfileID(text)}
-            style={styles.textInput}
-            label='Profile ID'
-            mode="outlined"
-            returnKeyType="done"
-            autoFocus
-            onSubmitEditing={submitProfile}
-            />
-            ) : (
-            <Button
-              mode="outlined"
-              style={styles.button}
-              labelStyle={styles.buttonLabel}
-              onPress={toggleNewProfile}
-            >
-              + Create Profile
-            </Button>
-        )}
-        </ScrollView>
-
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -96,62 +46,11 @@ export default App = () => {
 const getStyles = (theme) => ({
   safe: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: theme.colors.background,
   },
   container: {
-    marginTop: 20,
-    width: "90%",
-    padding: 10,
-    borderRadius: 20,
-    paddingBottom: 20,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    gap: 10,
   },
-  titleContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontFamily: FONT.OrbitronM,
-    fontSize: 50,
-    textAlign: "center",
-    marginVertical: 10,
-    color: theme.colors.onBackground,
-  },
-  scroll: {
-    width: "100%",
-    elevation: 5,
-    borderRadius: 20
-  },
-  scrollContent: {
-    backgroundColor: theme.colors.surface,
-    padding: 20,
-    borderRadius: 20
-  },
-  button: {
-    marginVertical: 5,
-    justifyContent: "center",
-  },
-  buttonLabel: {
-    height: 30,
-    textAlignVertical: "center",
-    lineHeight: 30,
-  },
-  buttonContent: {
-    justifyContent: "space-between",
-    flexDirection: "row-reverse"
-  },
-  iconContainer: {
-    width: 50,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: theme.colors.onPrimary,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-  },
-  textInput: {
-    marginVertical: 5,
-  }
 })
