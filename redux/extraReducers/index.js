1880e35745dc5f88b00b42d5e9798c80ac254ff3
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:23:46 -0600

    Change builder cases for async injected profiles

--- redux/extraReducers/index.js
+++ redux/extraReducers/index.js
@@ -17,174 +17,174 @@ import { templateOptionSlice } from "./templateOptionSlice";
 export const extraReducers = (builder) => {
   // Personal Slice Actions
   builder.addCase(personalSlice.actions.setPersonal, (state, action) => {
-    state.profiles[state.currentProfile].personal = personalSlice.reducer(state.profiles[state.currentProfile].personal, action);
+    state.personal = personalSlice.reducer(state.personal, action);
   });
 
   // Objective Slice Actions
   builder.addCase(objectiveSlice.actions.setObjective, (state, action) => {
-    state.profiles[state.currentProfile].objective = objectiveSlice.reducer(state.profiles[state.currentProfile].objective, action);
+    state.objective = objectiveSlice.reducer(state.objective, action);
   });
   builder.addCase(objectiveSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].objective = objectiveSlice.reducer(state.profiles[state.currentProfile].objective, action);
+    state.objective = objectiveSlice.reducer(state.objective, action);
   });
 
   // Summary Slice Actions
   builder.addCase(summarySlice.actions.setSummary, (state, action) => {
-    state.profiles[state.currentProfile].summary = summarySlice.reducer(state.profiles[state.currentProfile].summary, action);
+    state.summary = summarySlice.reducer(state.summary, action);
   });
   builder.addCase(summarySlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].summary = summarySlice.reducer(state.profiles[state.currentProfile].summary, action);
+    state.summary = summarySlice.reducer(state.summary, action);
   });
 
   // Highlight Slice Actions
   builder.addCase(highlightSlice.actions.addHighlight, (state, action) => {
-    state.profiles[state.currentProfile].highlights = highlightSlice.reducer(state.profiles[state.currentProfile].highlights, action);
+    state.highlights = highlightSlice.reducer(state.highlights, action);
   });
   builder.addCase(highlightSlice.actions.removeHighlight, (state, action) => {
-    state.profiles[state.currentProfile].highlights = highlightSlice.reducer(state.profiles[state.currentProfile].highlights, action);
+    state.highlights = highlightSlice.reducer(state.highlights, action);
   });
   builder.addCase(highlightSlice.actions.setHighlight, (state, action) => {
-    state.profiles[state.currentProfile].highlights = highlightSlice.reducer(state.profiles[state.currentProfile].highlights, action);
+    state.highlights = highlightSlice.reducer(state.highlights, action);
   });
   builder.addCase(highlightSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].highlights = highlightSlice.reducer(state.profiles[state.currentProfile].highlights, action);
+    state.highlights = highlightSlice.reducer(state.highlights, action);
   });
 
   // Experience Slice Actions
   builder.addCase(experienceSlice.actions.addJob, (state, action) => {
-    state.profiles[state.currentProfile].experience = experienceSlice.reducer(state.profiles[state.currentProfile].experience, action);
+    state.experience = experienceSlice.reducer(state.experience, action);
   });
   builder.addCase(experienceSlice.actions.removeJob, (state, action) => {
-    state.profiles[state.currentProfile].experience = experienceSlice.reducer(state.profiles[state.currentProfile].experience, action);
+    state.experience = experienceSlice.reducer(state.experience, action);
   });
   builder.addCase(experienceSlice.actions.setJob, (state, action) => {
-    state.profiles[state.currentProfile].experience = experienceSlice.reducer(state.profiles[state.currentProfile].experience, action);
+    state.experience = experienceSlice.reducer(state.experience, action);
   });
   builder.addCase(experienceSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].experience = experienceSlice.reducer(state.profiles[state.currentProfile].experience, action);
+    state.experience = experienceSlice.reducer(state.experience, action);
   });
 
   // Volunteer Slice Actions
   builder.addCase(volunteerSlice.actions.addPosition, (state, action) => {
-    state.profiles[state.currentProfile].volunteer = volunteerSlice.reducer(state.profiles[state.currentProfile].volunteer, action);
+    state.volunteer = volunteerSlice.reducer(state.volunteer, action);
   });
   builder.addCase(volunteerSlice.actions.removePosition, (state, action) => {
-    state.profiles[state.currentProfile].volunteer = volunteerSlice.reducer(state.profiles[state.currentProfile].volunteer, action);
+    state.volunteer = volunteerSlice.reducer(state.volunteer, action);
   });
   builder.addCase(volunteerSlice.actions.setPosition, (state, action) => {
-    state.profiles[state.currentProfile].volunteer = volunteerSlice.reducer(state.profiles[state.currentProfile].volunteer, action);
+    state.volunteer = volunteerSlice.reducer(state.volunteer, action);
   });
   builder.addCase(volunteerSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].volunteer = volunteerSlice.reducer(state.profiles[state.currentProfile].volunteer, action);
+    state.volunteer = volunteerSlice.reducer(state.volunteer, action);
   });
 
   // Education Slice Actions
   builder.addCase(educationSlice.actions.addEducation, (state, action) => {
-    state.profiles[state.currentProfile].education = educationSlice.reducer(state.profiles[state.currentProfile].education, action);
+    state.education = educationSlice.reducer(state.education, action);
   });
   builder.addCase(educationSlice.actions.removeEducation, (state, action) => {
-    state.profiles[state.currentProfile].education = educationSlice.reducer(state.profiles[state.currentProfile].education, action);
+    state.education = educationSlice.reducer(state.education, action);
   });
   builder.addCase(educationSlice.actions.setEducation, (state, action) => {
-    state.profiles[state.currentProfile].education = educationSlice.reducer(state.profiles[state.currentProfile].education, action);
+    state.education = educationSlice.reducer(state.education, action);
   });
   builder.addCase(educationSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].education = educationSlice.reducer(state.profiles[state.currentProfile].education, action);
+    state.education = educationSlice.reducer(state.education, action);
   });
 
   // Skill Slice Actions
   builder.addCase(skillSlice.actions.addSkill, (state, action) => {
-    state.profiles[state.currentProfile].skills = skillSlice.reducer(state.profiles[state.currentProfile].skills, action);
+    state.skills = skillSlice.reducer(state.skills, action);
   });
   builder.addCase(skillSlice.actions.removeSkill, (state, action) => {
-    state.profiles[state.currentProfile].skills = skillSlice.reducer(state.profiles[state.currentProfile].skills, action);
+    state.skills = skillSlice.reducer(state.skills, action);
   });
   builder.addCase(skillSlice.actions.setSkill, (state, action) => {
-    state.profiles[state.currentProfile].skills = skillSlice.reducer(state.profiles[state.currentProfile].skills, action);
+    state.skills = skillSlice.reducer(state.skills, action);
   });
   builder.addCase(skillSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].skills = skillSlice.reducer(state.profiles[state.currentProfile].skills, action);
+    state.skills = skillSlice.reducer(state.skills, action);
   });
   builder.addCase(skillSlice.actions.setUseProficiency, (state, action) => {
-    state.profiles[state.currentProfile].skills = skillSlice.reducer(state.profiles[state.currentProfile].skills, action);
+    state.skills = skillSlice.reducer(state.skills, action);
   });
 
   // Certificate Slice Actions
   builder.addCase(certificateSlice.actions.addCertificate, (state, action) => {
-    state.profiles[state.currentProfile].certificates = certificateSlice.reducer(state.profiles[state.currentProfile].certificates, action);
+    state.certificates = certificateSlice.reducer(state.certificates, action);
   });
   builder.addCase(certificateSlice.actions.removeCertificate, (state, action) => {
-    state.profiles[state.currentProfile].certificates = certificateSlice.reducer(state.profiles[state.currentProfile].certificates, action);
+    state.certificates = certificateSlice.reducer(state.certificates, action);
   });
   builder.addCase(certificateSlice.actions.setCertificate, (state, action) => {
-    state.profiles[state.currentProfile].certificates = certificateSlice.reducer(state.profiles[state.currentProfile].certificates, action);
+    state.certificates = certificateSlice.reducer(state.certificates, action);
   });
   builder.addCase(certificateSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].certificates = certificateSlice.reducer(state.profiles[state.currentProfile].certificates, action);
+    state.certificates = certificateSlice.reducer(state.certificates, action);
   });
 
   // Project Slice Actions
   builder.addCase(projectSlice.actions.addProject, (state, action) => {
-    state.profiles[state.currentProfile].projects = projectSlice.reducer(state.profiles[state.currentProfile].projects, action);
+    state.projects = projectSlice.reducer(state.projects, action);
   });
   builder.addCase(projectSlice.actions.removeProject, (state, action) => {
-    state.profiles[state.currentProfile].projects = projectSlice.reducer(state.profiles[state.currentProfile].projects, action);
+    state.projects = projectSlice.reducer(state.projects, action);
   });
   builder.addCase(projectSlice.actions.setProject, (state, action) => {
-    state.profiles[state.currentProfile].projects = projectSlice.reducer(state.profiles[state.currentProfile].projects, action);
+    state.projects = projectSlice.reducer(state.projects, action);
   });
   builder.addCase(projectSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].projects = projectSlice.reducer(state.profiles[state.currentProfile].projects, action);
+    state.projects = projectSlice.reducer(state.projects, action);
   });
 
   // Publication Slice Actions
   builder.addCase(publicationSlice.actions.addPublication, (state, action) => {
-    state.profiles[state.currentProfile].publications = publicationSlice.reducer(state.profiles[state.currentProfile].publications, action);
+    state.publications = publicationSlice.reducer(state.publications, action);
   });
   builder.addCase(publicationSlice.actions.removePublication, (state, action) => {
-    state.profiles[state.currentProfile].publications = publicationSlice.reducer(state.profiles[state.currentProfile].publications, action);
+    state.publications = publicationSlice.reducer(state.publications, action);
   });
   builder.addCase(publicationSlice.actions.setPublication, (state, action) => {
-    state.profiles[state.currentProfile].publications = publicationSlice.reducer(state.profiles[state.currentProfile].publications, action);
+    state.publications = publicationSlice.reducer(state.publications, action);
   });
   builder.addCase(publicationSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].publications = publicationSlice.reducer(state.profiles[state.currentProfile].publications, action);
+    state.publications = publicationSlice.reducer(state.publications, action);
   });
 
   // Language Slice Actions
   builder.addCase(languageSlice.actions.addLanguage, (state, action) => {
-    state.profiles[state.currentProfile].languages = languageSlice.reducer(state.profiles[state.currentProfile].languages, action);
+    state.languages = languageSlice.reducer(state.languages, action);
   });
   builder.addCase(languageSlice.actions.removeLanguage, (state, action) => {
-    state.profiles[state.currentProfile].languages = languageSlice.reducer(state.profiles[state.currentProfile].languages, action);
+    state.languages = languageSlice.reducer(state.languages, action);
   });
   builder.addCase(languageSlice.actions.setLanguage, (state, action) => {
-    state.profiles[state.currentProfile].languages = languageSlice.reducer(state.profiles[state.currentProfile].languages, action);
+    state.languages = languageSlice.reducer(state.languages, action);
   });
   builder.addCase(languageSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].languages = languageSlice.reducer(state.profiles[state.currentProfile].languages, action);
+    state.languages = languageSlice.reducer(state.languages, action);
   });
 
   // Reference Slice Actions
   builder.addCase(referenceSlice.actions.addReference, (state, action) => {
-    state.profiles[state.currentProfile].references = referenceSlice.reducer(state.profiles[state.currentProfile].references, action);
+    state.references = referenceSlice.reducer(state.references, action);
   });
   builder.addCase(referenceSlice.actions.removeReference, (state, action) => {
-    state.profiles[state.currentProfile].references = referenceSlice.reducer(state.profiles[state.currentProfile].references, action);
+    state.references = referenceSlice.reducer(state.references, action);
   });
   builder.addCase(referenceSlice.actions.setReference, (state, action) => {
-    state.profiles[state.currentProfile].references = referenceSlice.reducer(state.profiles[state.currentProfile].references, action);
+    state.references = referenceSlice.reducer(state.references, action);
   });
   builder.addCase(referenceSlice.actions.setActive, (state, action) => {
-    state.profiles[state.currentProfile].references = referenceSlice.reducer(state.profiles[state.currentProfile].references, action);
+    state.references = referenceSlice.reducer(state.references, action);
   });
 
   // Template Options Actions
   builder.addCase(templateOptionSlice.actions.setSize, (state, action) => {
-    state.profiles[state.currentProfile].options = templateOptionSlice.reducer(state.profiles[state.currentProfile].options, action);
+    state.options = templateOptionSlice.reducer(state.options, action);
   });
   builder.addCase(templateOptionSlice.actions.setOptions, (state, action) => {
-    state.profiles[state.currentProfile].options = templateOptionSlice.reducer(state.profiles[state.currentProfile].options, action);
+    state.options = templateOptionSlice.reducer(state.options, action);
   });
 }
 
