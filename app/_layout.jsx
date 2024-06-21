cd9dd463173909f5918edb11b7066cd0ceea3549
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-21 00:39:23 -0600

    Align header title center

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -12,7 +12,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { CombinedDefaultTheme, CombinedDarkTheme } from '../configs';
 import { FONT } from '../constants';
-import { HeaderBackButton } from '../components';
+import { HeaderBackButton, HeaderNextButton } from '../components';
 import { Icon } from '../components/icons/icon';
 import { StatusBar } from 'expo-status-bar';
 
@@ -34,10 +34,11 @@ export default function RootLayoutNav() {
         <PersistGate persistor={persistor} loading={<ActivityIndicator animating={true} color={theme.colors.error} size='large' />}>
           <Stack
             screenOptions={{
-              contentStyle: { backgroundColor: theme.colors.inverseOnSurface },
+              contentStyle: { backgroundColor: theme.colors.background },
               headerStyle: { backgroundColor: theme.colors.navbar},
               headerTintColor: theme.colors.onNavbarVariant,
               headerTitleStyle: { color: theme.colors.onNavbar, fontFamily: FONT.SairaB, fontSize: 22},
+              headerTitleAlign: "center",
               animation: "fade_from_bottom",
             }}
           >
